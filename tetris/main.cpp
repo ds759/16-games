af7fddd8fd144447f7a54df4cf57a074717a1f28
Daniel Savage <ds759@bath.ac.uk>
2019-07-23 22:09:34 +0100

    Created Window

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,1 +1,20 @@
+#include <SFML/Graphics.hpp>
 #include <time.h>
+
+using namespace sf;
+
+int main(){
+	RenderWindow window(VideoMode(320, 480), "The Game!");
+
+	Event e;
+	while(window.isOpen())
+	{
+		if(e.type == Event::Closed)
+			window.close();
+	}
+
+	window.clear(Color::White);
+	window.display();
+
+	return 0;
+}
